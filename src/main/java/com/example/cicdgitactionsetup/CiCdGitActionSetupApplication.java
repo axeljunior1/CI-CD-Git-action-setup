050d814373d8cf3c54cd009deb28075e2bddc173
axelmengue1 <axelmengue1@gmail.com>
2023-01-09 07:47:40 +0100

    start e

---
+++ src/main/java/com/example/cicdgitactionsetup/CiCdGitActionSetupApplication.java
@@ -0,0 +1,21 @@
+package com.example.cicdgitactionsetup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class CiCdGitActionSetupApplication {
+
+    @GetMapping("/")
+    String Greet() {
+        return "Hello Axel !";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(CiCdGitActionSetupApplication.class, args);
+    }
+
+}
